efa9bf49c3814a96ddf81b1b4060bbb85ede90b9
adel1234567546-dev <adel1234567546@gmail.com>
2026-04-01 19:07:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ async function getWisdom() {
     try {
         // 3. الاتصال بالباك إند (FastAPI)
         // ملاحظة: تأكد أن الرابط يطابق الذي يظهر في Terminal (127.0.0.1:8000)
-        const response = await fetch('https://your-link.onrender.com/get-wisdom', {
+       // غير هذا السطر
+const response = await fetch('https://elham-project.onrender.com/get-wisdom', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
